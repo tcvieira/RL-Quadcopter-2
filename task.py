e1c06a905d0305c4225ced9a59f32c7cdf4e2e79
tcvieira <thiagotcvieira@gmail.com>
2019-05-23 14:18:03 -0300

    update

--- task.py
+++ task.py
@@ -24,11 +24,13 @@ class Task():
         self.action_size = 4
 
         # Goal
-        self.target_pos = target_pos if target_pos is not None else np.array([0., 0., 10.]) 
+        self.target_pos = target_pos if target_pos is not None else np.array([0., 0., 0.])
+        self.target_velocity = 0. # My goal is to land, so the velocity should be 0. in the end
 
     def get_reward(self):
         """Uses current pose of sim to return reward."""
-        reward = 1.-.3*(abs(self.sim.pose[:3] - self.target_pos)).sum()
+        reward = 1.-.3*(np.linalg.norm(self.sim.pose[:3] - self.target_pos)) - .05*(np.linalg.norm(self.sim.pose[:2])/(0.05 + min(self.sim.pose[2],1)))
+
         return reward
 
     def step(self, rotor_speeds):
@@ -39,11 +41,15 @@ class Task():
             done = self.sim.next_timestep(rotor_speeds) # update the sim pose and velocities
             reward += self.get_reward() 
             pose_all.append(self.sim.pose)
+            self.poslist = np.append(self.poslist, [self.sim.pose], axis = 0)
         next_state = np.concatenate(pose_all)
         return next_state, reward, done
 
     def reset(self):
         """Reset the sim to start a new episode."""
+        self.sim.init_pose= self.init_pose
         self.sim.reset()
+        self.poslist = np.array([self.sim.pose])
+        noised_pose = self.sim.pose  # ADD NOISE AFTER TESTING
         state = np.concatenate([self.sim.pose] * self.action_repeat) 
         return state
